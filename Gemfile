db9cd1bfe67c99cd7e5f94ac7a8b8aa042a3b438
Tobi1246 <d.morox@ya.ru>
2023-11-18 15:16:09 +0300

    cloudflare s3

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'decent_exposure'
 gem 'turbolinks'
 gem 'webpacker'
 gem "aws-sdk-s3", require: false
+gem 'dotenv-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,16 +14,14 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//= require activestorage
 //= require jquery3
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
+require("channels")
+require("jquery")
 
 import "../utilities/answers.js"
 import "../utilities/questions.js"
